fca7c50def04204596cf939775c9d05e6a8d901c
IgniteCoders <theboom.kajas@gmail.com>
2025-06-02 12:55:17 +0200

    Trying to signUp, signIn and singOut with FirebaseAuth

---
+++ FirebaseApp/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.swift
+//  FirebaseApp
+//
+//  Created by Mañanas on 2/6/25.
+//
+
+import UIKit
+import FirebaseAuth
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signOut(_ sender: Any) {
+        do {
+          try Auth.auth().signOut()
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
